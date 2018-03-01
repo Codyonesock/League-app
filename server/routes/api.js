c7bc7ebb7226d221447f4ef016de4b6b19167d84
Cody Denny <cody.denny@redspace.com>
2018-03-01 16:57:14 -0400

    Set up single summoner end point.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const nano = require('nano')('http://localhost:5984');
+const { Kayn, REGIONS } = require('kayn')
+const { api_key } = require('../../key');
+
+const kayn = Kayn(api_key)({
+  region: REGIONS.NORTH_AMERICA,
+  debugOptions: {
+      isEnabled: true,
+      showKey: false,
+      loggers: {},
+  },
+  requestOptions: {
+      shouldRetry: true,
+      numberOfRetriesBeforeAbort: 3,
+      delayBeforeRetry: 1000,
+      burst: false,
+  },
+  cacheOptions: {
+      cache: null,
+      ttls: {},
+  },
+});
+
+var region, summonerName = '';
 
 // Specified databases
-const summoners = nano.db.use('summoners');
+const summonersDb = nano.db.use('summoners');
 
 let response = {
   response: 200,
-  data: []
+  data: [],
+  message: null
 };
 
 const sendError = (err, res) => {
@@ -16,8 +40,9 @@ const sendError = (err, res) => {
   res.status(501).json(response);
 };
 
+// Get all summoners
 router.get('/summoners', (req, res) => {
-  summoners.view('all_summoners', 'all', (err, body) => {
+  summonersDb.view('all_summoners', 'all', (err, body) => {
     if (err) {
       return sendError(err);
     }
@@ -26,6 +51,48 @@ router.get('/summoners', (req, res) => {
     res.json(response);
 
     return response;
+  });
+});
+
+// Get single summoner
+router.get(`/summoner/:user`, (req, res) => {
+  // TODO: Set up region handler
+  region = 'na1';
+  summonerName = req.params.user;
+  var summonerExist = false;
+
+  summonersDb.view('all_summoners', 'all', (err, body) => {
+    if (err) {
+      return sendError(err);
+    }
+
+    summonerExist = body.rows.filter(user => user.value.name.toLowerCase() === summonerName.toLowerCase());
+
+    if (Array.isArray(summonerExist) && summonerExist.length > 0) {
+      response.data = [summonerExist[0].value];
+      response.message = 'Data from DB';
+      res.json(response);
+
+      return response;
+    } else {
+      kayn.Summoner.by.name(summonerName).callback((err, data) => {
+        if (err) {
+          return sendError(err);
+        }
+
+        response.data = [data];
+        response.message = 'Data from Riot API';
+        res.json(response);
+
+        summonersDb.insert(response.data[0], (err, body, header => {
+          if (err) {
+            return sendError(err);
+          }
+        }));
+
+        return response;
+      });
+    }
   });
 });
 
